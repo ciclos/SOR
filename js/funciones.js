26db142d2200427e4d3bb72ca4d45259b8076838
Diego <diego.cordoba.a@gmail.com>
2020-01-07 10:33:44 +0100

    Active Directory nuevo

--- js/funciones.js
+++ js/funciones.js
@@ -1,20 +1,48 @@
 var mostrando = 0;
 ejercicios = document.querySelectorAll(".ejercicio");
 navegacion = document.querySelector("nav.navegacion");
-ejercicios[0].style.display = "block"; // Por defecto mostramos el primer ejercicio
 
-for (var i = 0; i < ejercicios.length; i++) {
-    navegacion.innerHTML += '<a onclick="cambiar(this)">' + (i + 1) + '</a>';
+if (ejercicios.length > 0) {
+    ejercicios[0].style.display = "block"; // Por defecto mostramos el primer ejercicio
 
+    for (var i = 0; i < ejercicios.length; i++) {
+        navegacion.innerHTML += '<a onclick="cambiar(this)">' + (i + 1) + '</a>';
+
+    }
+    document.querySelector("nav.navegacion").childNodes[1].style.backgroundColor = "red";
+
+    function cambiar(enlace) {
+        indice = (enlace.innerHTML - 1);
+        botones = document.querySelectorAll(".ejercicio a");
+        ejercicios[mostrando].style.display = "none";
+        ejercicios[indice].style.display = "block";
+        document.querySelector("nav.navegacion").childNodes[indice + 1].style.backgroundColor = "red";
+        document.querySelector("nav.navegacion").childNodes[mostrando + 1].style.backgroundColor = "#003f69";
+        mostrando = indice;
+    }
 }
-document.querySelector("nav.navegacion").childNodes[1].style.backgroundColor = "red";
 
-function cambiar(enlace) {
-    indice = (enlace.innerHTML - 1);
-    botones = document.querySelectorAll(".ejercicio a");
-    ejercicios[mostrando].style.display = "none";
-    ejercicios[indice].style.display = "block";
-    document.querySelector("nav.navegacion").childNodes[indice + 1].style.backgroundColor = "red";
-    document.querySelector("nav.navegacion").childNodes[mostrando + 1].style.backgroundColor = "#003f69";
-    mostrando = indice;
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+
+imagenes = document.querySelectorAll("img");
+
+for (var i = 0; i < imagenes.length; i++) {
+    imagenes[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    };
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
 }
